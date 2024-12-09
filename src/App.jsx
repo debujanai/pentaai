0e98bd8a65559ca1cf2b03441c57736281717ddd
Luka <alutza94@gmail.com>
2024-12-09 12:50:05 +0100

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>aaa</div>;
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">AAAfAAAAAA</h1>
+    </main>
+  );
 };
 
 export default App;
