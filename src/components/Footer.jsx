b809e5c7b7eac6b84519d941d7c181cf8c23af7d
Luka <alutza94@gmail.com>
2024-12-17 13:03:47 +0100

    add default footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <div>f</div>;
+  return (
+    <footer className="w-screen bg-violet-500 py-4 text-white">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">
+          &copy;{" "}
+          <a
+            href="https://www.semper.blue/"
+            target="_blank"
+            className="hover:text-red-500 hover:underline"
+          >
+            Semper
+          </a>{" "}
+          2024. All rights reserved
+        </p>
+        <div className="flex justify-center gap-4 md:justify-start"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
