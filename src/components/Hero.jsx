55e787fdd8c22494e12a39b8b67da16aea19534c
Luka <alutza94@gmail.com>
2024-12-19 18:37:15 +0100

    minor fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -83,7 +83,7 @@ const Hero = () => {
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
-    <div className="relative h-dvh w-screen overflox-x-hidden">
+    <div className="relative h-dvh w-screen overflow-x-hidden">
       {isLoading && (
         <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
           <div className="three-body">
