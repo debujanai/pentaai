d51cec63e8d2c913e57d4f7b53a970e7b414f367
debujanai <kashifjalal01@gmail.com>
2025-01-25 23:20:01 +0500

    features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -97,7 +97,7 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
 };
 
 const Features = () => (
-  <section id="agents" className="bg-black pb-52">
+  <section id="agents" className="bg-black pb-10 md:pb-52">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
@@ -118,7 +118,7 @@ const Features = () => (
         />
       </BentoTilt>
 
-      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+      <div className="grid h-[95vh] md:h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
             src="img/r2.png"
